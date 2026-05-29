32694f28d80cfdd5393aa3c7a31e2ae149161e44
Jesus Calixto <jcalixto@ciemsa.com.uy>
2026-05-28 21:26:37 -0300

    Complete l5-task2

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -4,11 +4,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
 #include <stdio.h>
+
+static int board_init(void)
+{
+	printf("Board Initialized\n");
+	return 0;
+}
+
+/* Ejecuta antes de main() */
+SYS_INIT(board_init, APPLICATION, 0);
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
 	return 0;
 }
